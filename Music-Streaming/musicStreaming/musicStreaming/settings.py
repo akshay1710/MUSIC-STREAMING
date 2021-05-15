67c5e483a1979c5038500b689ac580398d332ba2
akshay-biosense <akshay.vadkar@biosense.in>
2021-05-15 11:15:31 +0530

    15/05

--- Music-Streaming/musicStreaming/musicStreaming/settings.py
+++ Music-Streaming/musicStreaming/musicStreaming/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CONTENT_DIR = BASE_DIR / 'content'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -55,7 +57,9 @@ ROOT_URLCONF = 'musicStreaming.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+               CONTENT_DIR /'template'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,10 +121,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = CONTENT_DIR /'assert'
 STATIC_URL = '/static/'
+STATICFILES_DIR = CONTENT_DIR /'assert'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+# htps://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
